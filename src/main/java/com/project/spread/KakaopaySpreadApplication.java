be85f4cdd2d39bc813999e3b77fb8dc48a615834
dphong <dphong@kaoni.com>
2020-11-17 10:45:00 +0900

    kakaopay prestest first commit

---
+++ src/main/java/com/project/spread/KakaopaySpreadApplication.java
@@ -0,0 +1,13 @@
+package com.project.spread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopaySpreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaopaySpreadApplication.class, args);
+	}
+
+}
