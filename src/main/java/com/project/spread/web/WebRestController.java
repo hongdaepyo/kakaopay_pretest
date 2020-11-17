db0057e89590770a9ade4fba53fc45eea3fa6da5
dphong <dphong@kaoni.com>
2020-11-17 18:00:56 +0900

    added controller method

--- src/main/java/com/project/spread/web/WebRestController.java
+++ src/main/java/com/project/spread/web/WebRestController.java
@@ -3,11 +3,26 @@ package com.project.spread.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("kakaopay")
 public class WebRestController {
 	
 	@GetMapping("/hello")
 	public String hello() {
+		return "helloWorld";
+	}
+	
+	@GetMapping("/spreadMoney")
+	public String spreadMoney() {
+		return "helloWorld";
+	}
+	
+	@GetMapping("/acceptMoney")
+	public String acceptMoney() {
+		return "helloWorld";
+	}
+	
+	@GetMapping("/checkMoney")
+	public String checkMoney() {
 		return "helloWorld";
 	}
 }
