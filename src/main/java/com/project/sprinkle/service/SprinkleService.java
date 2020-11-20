bb3db611bd565ca95d1825f843e2a9302471fea2
dphong <dphong@kaoni.com>
2020-11-20 15:31:23 +0900

    뿌리기 저장 로직 뿌리기 서비스에 추가.

--- src/main/java/com/project/sprinkle/service/SprinkleService.java
+++ src/main/java/com/project/sprinkle/service/SprinkleService.java
@@ -1,7 +1,13 @@
 package com.project.sprinkle.service;
 
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.sprinkle.domain.sprinkle.Sprinkle;
 import com.project.sprinkle.dto.SprinkleSaveRequestDto;
@@ -10,20 +16,25 @@ import com.project.sprinkle.util.CommonUtil;
 @Service
 public class SprinkleService {
 	
+	@PersistenceContext
+	private EntityManager em;
+	
 	@Autowired
 	private CommonUtil commonUtil;
 	
 	public Sprinkle[] toEntities(SprinkleSaveRequestDto dto) {
 		int memberCount = dto.getMemberCount();
 		Sprinkle[] sprinkleArr = new Sprinkle[memberCount];
+		String token = commonUtil.generateToken();
+		
 		for (int i = 0; i < memberCount; i++) {
-			String token = commonUtil.generateToken();
 			sprinkleArr[i] = Sprinkle.builder()
 								.dividedAmount(dto.getAmount() / memberCount)
 								.roomId(dto.getRoomId())
 								.token(token)
 								.tokenSN(i)
 								.sprinkleUserId(dto.getSprinkleUserId())
+								.sprinkleDate(new Date().toString())
 								.build();
 		}
 		
@@ -35,5 +46,19 @@ public class SprinkleService {
 		dto.setRoomId(roomId);
 		
 		return toEntities(dto);
+	}
+	
+	@Transactional
+	public String save(Sprinkle[] sprinkleArr) {
+		String token = sprinkleArr[0].getToken();
+		
+		for (int i = 0; i < sprinkleArr.length; i++) {
+			em.persist(sprinkleArr[i]);
+		}
+		
+		em.flush();
+		em.clear();
+		
+		return token;
 	}
 }
--- src/main/java/com/project/sprinkle/web/WebRestController.java
+++ src/main/java/com/project/sprinkle/web/WebRestController.java
@@ -2,10 +2,10 @@ package com.project.sprinkle.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.sprinkle.domain.sprinkle.Sprinkle;
@@ -28,15 +28,15 @@ public class WebRestController {
 	}
 	
 	@PostMapping(value = "/sprinkle", produces="application/json;charset=utf-8")
-	public String sprinkleMoney(@RequestBody SprinkleSaveRequestDto dto, HttpServletRequest request) {
+	public ResponseEntity<String> sprinkleMoney(@RequestBody SprinkleSaveRequestDto dto, HttpServletRequest request) {
 		String userId = request.getHeader("X-USER-ID");
 		String roomId = request.getHeader("X-ROOM-ID");
 		
 		Sprinkle[] sprinkleArr = sprinkleService.toEntities(userId, roomId, dto);
 		
-		// 저장하고 토큰 리턴하는 로직 구현해야함.
+		String returnToken = sprinkleService.save(sprinkleArr);
 		
-		return "true";
+		return ResponseEntity.ok(returnToken);
 	}
 	
 	@GetMapping("/acceptMoney")
